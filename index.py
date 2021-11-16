9ba99797fbcdffca801d3d7534ab95fb07545106
Amalmalik Abuzarov <amalmalikredrobin@gmail.com>
2021-11-16 17:56:57 +0300

    last document 16.11.2021

--- index.py
+++ index.py
@@ -1,6 +1,22 @@
-age = int(input('Vvedite svoy vozrast'))
+# age = int(input('Vvedite svoy vozrast'))
 
-if age >= 18 and age <= 50:
-    print('Welcome')
-else: print('Bye')
+# if age >= 18 and age <= 50:
+#     print('Welcome')
+# else: print('Bye')
 
+cost=int(input("vvedite stoimost azakaza: "))
+money=int(input("vvedite summu kotoruyu vy dali: "))
+
+if cost % 100 == 0 and money % 100 ==0:
+     change = money - cost
+     five_hundreds = change // 500
+     change = money - cost
+
+     two_hundreds= change // 200
+     change = change % 200
+
+     hundreds = change // 100
+     print(five_hundreds, two_hundreds, hundreds)
+
+
+print(500//5)
